0bbe33e3fbf47bba349ce2d3326ce822462b93a5
Razvan G <razvan.gir@gmail.com>
2014-11-16 17:08:02 +0200

    UML

--- src/tema5/Main.java
+++ src/tema5/Main.java
@@ -19,12 +19,18 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
+		//Created a DrinkFactory object using the FactoryProducer
 		AbstractFactory drinkFactory1 = FactoryProducer.getFactory("drink");
+		//Created a Beer object using the DrinkFactory instantiated earlier
 		IDrink drink1 = drinkFactory1.getDrink("beer");
+		//Called the method prepareDrinks implemented from IDrink
 		drink1.prepareDrink();
 		
+		//Created a FoodFactory object using the FactoryProducer
 		AbstractFactory foodFactory1 = FactoryProducer.getFactory("food");
+		//Created a Mexican object using the FoodFactory instantiated earlier
 		IFood food1 = foodFactory1.getFood("mexican");
+		//Called the method cookFood implemented from IFood
 		food1.cookFood();
 	}
 
